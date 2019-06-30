bd1e1a0dd3f2cc1318742ee3529844621b09fee6
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-06-30 16:40:36 -0600

    calculator buttons and diplay made.

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const numberInput = [];
+
+function numberDisplay(event){
+    event.preventDefault()
+    const numberInput = $("#calculate").val()
+    numberInput.push(numberInput);
+    $('#calculate').val("");
+
+
+$('#enter').on('click', submit(e){
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
